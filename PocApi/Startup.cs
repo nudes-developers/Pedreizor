ae324d5b8dfa7c48c0dd08deec0ec123ee0b2406
Patrick Ens <patrick.ens@sp.senai.br>
2021-08-16 13:48:24 -0300

    updating pedreizor

--- PocApi/Startup.cs
+++ PocApi/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Nudes.Pedreizor;
 using Nudes.Pedreizor.Configuration;
 
@@ -23,18 +24,18 @@ namespace PocApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                routes.MapDefaultControllerRoute();
             });
         }
     }
